e26388338a9ad9750b527fcacc5443f5015a086f
AFOLABI AYODELE A <elkanahomonitan@gmail.com>
2019-11-02 08:58:57 +0100

    refactor(dockerfile): change ownership to projects

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -24,6 +24,8 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
     public void createMapIntent(View view){
         Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
@@ -33,10 +35,14 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
-    // ***
     // TODO - Task 3 - Launch the Phone Activity
+    // ***
+    public void createPhoneIntent(View view)
+    {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
+    }
+
     // ***
 }
